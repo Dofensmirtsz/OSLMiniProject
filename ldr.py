b373c4c320e5c364db665dd4e25948e9e530a72e
Dofensmirtsz <dofensmirtsz@gmail.com>
2018-03-26 13:42:49 +0200

    ver1

---
+++ ldr.py
@@ -0,0 +1,70 @@
+#!/usr/local/bin/python
+
+import RPi.GPIO as GPIO
+import time, json
+
+GPIO.setmode(GPIO.BCM)
+
+#define the pin that goes to the circuit
+pin_to_circuit = 4
+
+#define status of light
+lightOn  = False;
+
+def rc_time (pin_to_circuit):
+    count = 0
+
+    #Output on the pin for
+    GPIO.setup(pin_to_circuit, GPIO.OUT)
+    GPIO.output(pin_to_circuit, GPIO.LOW)
+    time.sleep(0.1)
+
+    #Change the pin back to input
+    GPIO.setup(pin_to_circuit, GPIO.IN)
+
+    #Count until the pin goes high
+    while (GPIO.input(pin_to_circuit) == GPIO.LOW):
+        count += 1
+
+    return count
+
+
+def ledOn ():
+   GPIO.setwarnings(False)
+   GPIO.setup(18,GPIO.OUT)
+   print "LED on"
+   GPIO.output(18,GPIO.HIGH)
+
+def ledOff ():
+   print "LED off"
+   GPIO.setup(18,GPIO.OUT)
+   GPIO.output(18,GPIO.LOW)
+
+def lightStatus():
+    jsonFile = open("C:/mydir/lightData.json","r")
+    data = json.load(jsonFile)
+    jsonFile.close
+    value = data["lights"][0]["status"]
+    print value
+    return value
+
+#Catch when script is interrupted, cleanup correctly
+try:
+    # Main loop
+    while True:
+        lightOn = lightStatus()
+
+        if(lightOn):
+            count = rc_time(pin_to_circuit)
+            print count
+            if(count > 666):
+                ledOn()
+            else:
+                ledOff()
+        else:
+            print("light not on")
+
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
